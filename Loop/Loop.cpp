57da424bcf62791e255992b99b6ba7b71d37843d
setha <sethasith123@gmail.com>
2025-03-04 15:58:16 +0700

    update

--- Loop/Loop.cpp
+++ Loop/Loop.cpp
@@ -179,7 +179,7 @@ int main()
 
 */
 // exercise exchange amount dollar to ...
-/*
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -187,127 +187,106 @@ int main(){
     int options;
     float dollar,riel,baht,won,yuan,yen,rupee,euro,peso,exchange;
     do{
-        cout<<"Welcome to ours program for exchange amount"<<endl;
-        cout<<"1. Dollar to Riel"<<endl;
-        cout<<"2. Dollar to Baht"<<endl;
-        cout<<"3. Dollar to Won"<<endl;
-        cout<<"4. Dollar to Yuan"<<endl;
-        cout<<"5. Dollar to Yen"<<endl;
-        cout<<"6. Dollar to Rupee"<<endl;
-        cout<<"7. Dollar to Euro"<<endl;
-        cout<<"8. Dollar to Peso"<<endl;
-        cout<<"0. Exit"<<endl;
-        cout<<"Enter your choice: ";
+        cout<<"=============================[Menu For Exchange]============================="<<endl;
+        cout<<"1. Exchange Dollar to Riel."<<endl;
+        cout<<"2. Exchange Dollar to Baht."<<endl;
+        cout<<"3. Exchange Dollar to Won."<<endl;
+        cout<<"4. Exchange Dollar to Yen."<<endl;
+        cout<<"5. Exchange Dollar to Yuan."<<endl;
+        cout<<"6. Exchange Dollar to Euro."<<endl;
+        cout<<"7. Exchange Dollar to Rupee."<<endl;
+        cout<<"8. Exchange Dollar to Peso."<<endl;
+        cout<<"9. Exit ( 0 )"<<endl;
+        cout<<"Please choosing an option : ";
         cin>>options;
-        switch(options){
-            case 0 :{
-                cout<<"================================================"<<endl;
-                cout<<"             Exiting the program...             "<<endl;
-                cout<<"================================================"<<endl;
-                break;
-            }
-            case 1 :{
-                riel = 4000.0;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Riel."<<endl;
-                cout<<"Enter the amount of dollars: ";
-                cin>>dollar;
-                exchange = dollar * riel;  
-                cout<<"The amount in Riel is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
 
+        switch(options){
+            case 0 : {
+                cout<<"=============================[Exiting the program]============================="<<endl;
                 break;
             }
-            case 2 :{
-                baht = 33.06;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Baht."<<endl;
-                cout<<"Enter the amount of dollars: ";
+            case 1 : {
+                riel = 4000.00;
+                cout<<"===============================[ You are select Dollar to Riel ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * baht;  
-                cout<<"The amount in Baht is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * riel;
+                cout<<"Cost Riel after exchange : "<<exchange<<endl;
                 break;
             }
-            case 3 :{
+            case 2 : {
+                baht = 35.06;
+                cout<<"===============================[ You are select Dollar to Baht ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
+                cin>>dollar;
+                exchange = dollar * baht;
+                cout<<"Cost Baht after exchange : "<<exchange<<endl;
+                break;
+            }
+            case 3 : {
                 won = 1404.54;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Won."<<endl;
-                cout<<"Enter the amount of dollars: ";
+                cout<<"===============================[ You are select Dollar to Won ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * won;  
-                cout<<"The amount in Won is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * won;
+                cout<<"Cost Won after exchange : "<<exchange<<endl;
                 break;
             }
             case 4 :{
-                yuan = 7.15;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Yuan."<<endl;
-                cout<<"Enter the amount of dollars: ";
+                yen = 150.18;
+                cout<<"===============================[ You are select Dollar to Yen ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * yuan;  
-                cout<<"The amount in Yuan is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * yen;
+                cout<<"Cost Yen after exchange : "<<exchange<<endl;
                 break;
             }
             case 5 :{
-                yen = 150.18;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Yen."<<endl;
-                cout<<"Enter the amount of dollars: ";
+                yuan = 7.15;
+                cout<<"===============================[ You are select Dollar to Yuan ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * yen;  
-                cout<<"The amount in Yen is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * yuan;
+                cout<<"Cost Yuan after exchange : "<<exchange<<endl;
                 break;
             }
-            case 6 :{
-                rupee = 278.20;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Rupee."<<endl;
-                cout<<"Enter the amount of dollars: ";
-                cin>>dollar;
-                exchange = dollar * rupee;  
-                cout<<"The amount in Rupee is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
-                break;
-            }
-            case 7 :{
+            case 6 : {
                 euro = 0.94;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Euro."<<endl;
-                cout<<"Enter the amount of dollars: ";
+                cout<<"===============================[ You are select Dollar to Euro ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * euro;  
-                cout<<"The amount in Euro is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * euro;
+                cout<<"Cost Euro after exchange : "<<exchange<<endl;
+                break;
+
+            }
+            case 7 : {
+                rupee = 278.20;
+                cout<<"===============================[ You are select Dollar to Rupee ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
+                cin>>dollar;
+                exchange = dollar * rupee;
+                cout<<"Cost Rupee after exchange : "<<exchange<<endl;
                 break;
             }
-            case 8 :{
+            case 8 : {
                 peso = 57.79;
-                cout<<"================================================"<<endl;
-                cout<<"You are choosing exchange Dollar to Peso."<<endl;
-                cout<<"Enter the amount of dollars: ";
+                cout<<"===============================[ You are select Dollar to Peso ]=============================="<<endl;
+                cout<<"Please enter cost Dollar for exchange :";
                 cin>>dollar;
-                exchange = dollar * peso;  
-                cout<<"The amount in Peso is: "<<exchange<<endl;
-                cout<<"================================================"<<endl;
+                exchange = dollar * peso;
+                cout<<"Cost Peso after exchange : "<<exchange<<endl;
                 break;
             }
-            default:{
-                cout<<"================================================"<<endl;
-                cout<<"             Invalid option. Try again.         "<<endl;
-                cout<<"================================================"<<endl;
+            default : {
+                cout<<"=============================[Invalid Option]=============================="<<endl;
+                cout<<"Invalid choice. Please try again."<<endl;
                 break;
-                
             }
-
         }
+        
 
-    }while(options != 0);
+    }while (options != 0);
+
+    return 0;
 }
-
-
-
-*/
